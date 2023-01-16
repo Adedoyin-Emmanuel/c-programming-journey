fbef8f01e3283130ec99190e13d8e659f6f0a86a
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-16 18:07:22 +0100

    Added the main.c code

---
+++ c-argv-argc/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the argument count and the argument vector
+ * @argc: the argument count parameter
+ * @argv: the argument vector parameter
+*/
+
+int main(int argc, int *argv[])
+{
+    printf("%d\n",argc);
+}
