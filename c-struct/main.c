73364b2066c32b4c724332a2d85cbab7b2c955d2
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-19 21:01:06 +0100

    Bug fix

--- c-struct/main.c
+++ c-struct/main.c
@@ -5,13 +5,20 @@ struct User
     char name[20];
     int age;
     char address[50];
-
 };
 
-
 int main(void)
-{
-    struct User user1 = {"Adedoyin Emmanuel",17,"42, kuburat Agbeduyi street"};
-
-    printf("My name is %s, I am %d years old. I live at %s\n", user1.name, user1.age, user1.address);
+{   
+    //create a group of users
+    struct User users_group[8] = {
+        {"Adedoyin Josiah", 80, "42, kuburat Agbeduyi street"},
+        {"Adedoyin Adefunke", 50, "42, kuburat Agbeduyi street"},
+        {"Adedoyin Aderonke", 25, "42, kuburat Agbeduyi street"},
+        {"Adedoyin Joshua", 23, "42, kuburat Agbeduyi street"},
+        {"Adedoyin Adeola", 20, "42, kuburat Agbeduyi street"},
+        {"Adedoyin Emmanuel", 17, "42, kuburat Agbeduyi street"},
+        {"Adedoyin Adewumi", 13, "42, kuburat Agbeduyi street"},
+        {"Adedoyin Israel", 11, "42, kuburat Agbeduyi street"}
+    };
+    return 0;
 }
