4a6dfca8c4f889ef8ed8bfac985e5fae592801e6
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-19 21:42:49 +0100

    Added the exe file

--- c-struct/main.c
+++ c-struct/main.c
@@ -8,24 +8,25 @@ struct User
 };
 
 int main(void)
-{   
-    //create a group of users
-    struct User users_group[8] = 
+{
+    // create a group of users
+    struct User users_group[8] =
     {
-        {"Adedoyin Josiah", 80, "42, kuburat Agbeduyi street"},
-        {"Adedoyin Adefunke", 50, "42, kuburat Agbeduyi street"},
-        {"Adedoyin Aderonke", 25, "42, kuburat Agbeduyi street"},
-        {"Adedoyin Joshua", 23, "42, kuburat Agbeduyi street"},
-        {"Adedoyin Adeola", 20, "42, kuburat Agbeduyi street"},
-        {"Adedoyin Emmanuel", 17, "42, kuburat Agbeduyi street"},
-        {"Adedoyin Adewumi", 13, "42, kuburat Agbeduyi street"},
-        {"Adedoyin Israel", 11, "42, kuburat Agbeduyi street"}
+            {"Adedoyin Josiah", 80, "42, kuburat Agbeduyi street"},
+            {"Adedoyin Adefunke", 50, "42, kuburat Agbeduyi street"},
+            {"Adedoyin Aderonke", 25, "42, kuburat Agbeduyi street"},
+            {"Adedoyin Joshua", 23, "42, kuburat Agbeduyi street"},
+            {"Adedoyin Adeola", 20, "42, kuburat Agbeduyi street"},
+            {"Adedoyin Emmanuel", 17, "42, kuburat Agbeduyi street"},
+            {"Adedoyin Adewumi", 13, "42, kuburat Agbeduyi street"},
+            {"Adedoyin Israel", 11, "42, kuburat Agbeduyi street"}
+            
     };
 
-    for(int i = 0; i < sizeof(users_group); i++)
+    for (int i = 0; i < (sizeof(users_group) / sizeof(struct User)); i++)
     {
-        printf("My name is %s, I am %d years old, I live at %s", users_group[i].name, users_group[i].age, users_group[i].address);
+        printf("My name is %s, I am %d years old, I live at %s\n", users_group[i].name, users_group[i].age, users_group[i].address);
     }
 
-    return 0;
+    return (0);
 }
