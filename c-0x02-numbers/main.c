a8c6691a4f4d82244eb46eebbb9a5d671395e832
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-25 17:55:34 +0100

    Updated the numbers dir

---
+++ c-0x02-numbers/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+    int num_1 = 50, num_2 = 20;
+
+    //Addition
+
+    printf("%d", num_1 + num_2);
+
+
+    //Subtraction
+
+    printf("%d", num_1 - num_2);
+
+    
+    //Multiplication
+
+    printf("%d", num_1 * num_2);
+
+
+    //Division
+
+    printf("%d", num_1 / num_2);
+
+    return (0);
+}
