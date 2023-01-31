f3b4d2c4f795f491960083a64653734c0a873021
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-31 17:37:11 +0100

    Added the second task

---
+++ c-argv-argc/projects/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - print a number followed by a new line
+ *@argc: this is the parameter for the argument count
+ *@argv: this is the parameter for the argument vector which is a charcter pointer array
+ */
+
+void main(int argc, char *argv[])
+{
+	printf("%d\n",argc);
+}
