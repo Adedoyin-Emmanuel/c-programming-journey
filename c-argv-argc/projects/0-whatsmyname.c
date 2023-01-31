9793b02acc7f70d9a1f97416d6fe6c37e6639442
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-31 17:26:31 +0100

    Finished the 0-whatsmyname.c file

--- c-argv-argc/projects/0-whatsmyname.c
+++ c-argv-argc/projects/0-whatsmyname.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 /**
- *main - print the program name
+ *main - prints the program name followed by a new line
+ *@argc: the argument count parameter
+ *@argv[]: the argument vector array parameter
+ *Return: 0 on success
  */
+
+int main(int argc, char *argc[])
+{
+	printf(argv[agrc]\n);
+	return (0);
+}
+
