bab110ee4ccb27409055fe07539f6f63d4350027
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-31 19:13:19 +0100

    bug fix

--- c-argv-argc/projects/4-add.c
+++ c-argv-argc/projects/4-add.c
@@ -7,3 +7,30 @@
  *Return: returns 1 on error
  */
 
+int main(int argc, char *argv[])
+{
+
+	int i, result = 0;
+
+	/* check if no argument was passed into the main function*/
+	if ( !(argc > 1))
+	{
+		printf("1\n");
+		return (1);
+	}
+
+	/*check if the argument is of positive values only*/
+	for (i = 0; i < argc; i++)
+	{
+		if (argv[i] < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		result += atoi(argv[i]);
+	}
+
+	printf("%d", result);
+
+}
