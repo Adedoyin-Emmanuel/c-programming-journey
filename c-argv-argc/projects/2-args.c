c17528c01f85f36c512f373caff7979fc70057f4
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-31 17:42:20 +0100

    Added the third task

---
+++ c-argv-argc/projects/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *main - prints all the arguments it receives during execution
+ *@argc: this is the argument count parameter
+ *@argv: this is the argument vector parameter which is a character pointer array
+ */
+
+void main (int argc, char *argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s",argv[i]);
+	}
+}
