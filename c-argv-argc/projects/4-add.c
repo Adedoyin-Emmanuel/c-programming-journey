e61b36addbba645435fbdbb1f078c478f6323177
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-31 19:06:34 +0100

    Added the 5th task 4-add.c

---
+++ c-argv-argc/projects/4-add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*
+ *main - prints positive numbers 
+ *@argc: this is the argument count parameter
+ *@argv: this is the argument vector parameter which is a character pointer array
+ *Return: returns 1 on error
+ */
+
